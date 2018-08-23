9ea68bf9125b69fbeddb8121ea892e60a8f84499
younghacker <godfreymbuva@gmail.com>
2018-08-24 00:24:10 +0300

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,15 +1,18 @@
 #!/bin/bash
 clear
+echo "Designed by MRX-love me"
+echo ""
+echo " %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%"
 echo " ROOTHACK4R, TROJAN, MRX, ROOT_EXPLOIT, MAST3R_MIND"
-
+echo " %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%"
 echo "[✔] Checking directories...";
-if [ -d "/usr/share/doc/Crips" ] ;
+if [ -d "/usr/share/doc/MRX" ] ;
 then
-echo "[◉] A directory Crips was found! Do you want to replace it? [Y/n]:" ; 
+echo "[◉] A directory MRX was found! Do you want to replace it? [Y/n]:" ; 
 read mama
 if [ $mama == "y" ] ; 
 then
- rm -R "/usr/share/doc/Crips"
+ rm -R "/usr/share/doc/MRX"
 else
  exit
 fi
@@ -17,21 +20,23 @@ fi
 
  echo "[✔] Installing ...";
  echo "";
- git clone https://github.com/Manisso/Crips.git /usr/share/doc/Crips;
+ git clone https://github.com/Manisso/Trojan.git /usr/share/doc/MRX;
  echo "#!/bin/bash 
- python /usr/share/doc/Crips/crips.py" '${1+"$@"}' > crips;
- chmod +x crips;
- sudo cp crips /usr/bin/;
- rm crips;
+ python /usr/share/doc/MRX/MRX.py" '${1+"$@"}' > crips;
+ chmod +x MRX;
+ sudo cp MRX /usr/bin/;
+ rm MRX;
 
 
-if [ -d "/usr/share/doc/Crips" ] ;
+if [ -d "/usr/share/doc/MRX" ] ;
 then
 echo "";
-echo "[✔]Tool istalled with success![✔]";
+echo "[✔]Tool istalled with success!==>Ready to Attack[✔]";
 echo "";
+echo "Love MRX-"
+echo"greatings to all HACKERHUB MEMBERS"
   echo "[✔]====================================================================[✔]";
-  echo "[✔] ✔✔✔  All is done!! You can execute tool by typing crips  !     ✔✔✔ [✔]"; 
+  echo "[✔] ✔✔✔  All is done!! You can execute tool by typing MRX  !     ✔✔✔ [✔]"; 
   echo "[✔]====================================================================[✔]";
   echo "";
 else
